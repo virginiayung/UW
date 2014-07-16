e39731d93a89da9b08e206146d1d5255467946d9
virginiayung <yungvirginia03@gmail.com>
2014-07-16 01:46:48 -0700

    Removing Zipfian Files From This Repo

--- Python_Projects/Zipfian_24hr/Zipfian_q3.sql
+++
@@ -1,7 +0,0 @@
-select S.ID as Salesperson_ID, S.Name, S.Age, S.Salary, Count(O.Number) as Num_Order, Sum(O.Amount)as Total_Amount
-from Salesperson S
-Inner Join Orders O on S.ID = O.salesperson_id
-Group by O.salesperson_id 
-Having Num_Order > 1;
-
-
